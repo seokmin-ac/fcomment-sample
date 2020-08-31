08a2fc7dd893b65ec5e6499c161453a7dbf1a9c2
Seokmin Hong <kyma123@unist.ac.kr>
2020-09-01 01:25:48 +0900

    Add comments component

---
+++ src/components/comments.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Comments = () => {
+  return (
+    <div>
+      <div style={{ fontFamily: "Montserrat,sans-serif", fontSize: "1.5rem" }}>
+        Comments
+      </div>
+    </div>
+  )
+}
+
+export default Comments
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Comments from "../components/comments"
 import { rhythm, scale } from "../utils/typography"
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
@@ -45,6 +46,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         />
         <footer>
           <Bio />
+          <Comments />
         </footer>
       </article>
 
