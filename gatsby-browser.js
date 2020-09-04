68687be92162240ea111a5abf52aec4e6dd8bae5
Seokmin Hong <kyma123@unist.ac.kr>
2020-09-04 10:40:38 +0900

    Rename env

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,13 +11,11 @@ import "prismjs/themes/prism.css"
 const onRedirectCallback = appState => navigate(appState?.returnTo || "/")
 
 export const wrapRootElement = ({ element }) => {
-  console.log(`AUTH0_DOMAIN: ${process.env.AUTH0_DOMAIN}`)
-  console.log(`GATSBY_DOMAIN: ${process.env.GATSBY_DOMAIN}`)
   return (
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN}
-      clientId={process.env.AUTH0_CLIENT_ID}
-      audience={process.env.AUTH0_AUDIENCE}
+      domain={process.env.AUTH_DOMAIN}
+      clientId={process.env.AUTH_CLIENT_ID}
+      audience={process.env.AUTH_AUDIENCE}
       scope="read:users"
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
