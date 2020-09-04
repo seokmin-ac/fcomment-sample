9c3a5fc84eff8f7e5e1e1e098e268822051e8ff2
Seokmin Hong <kyma123@unist.ac.kr>
2020-09-04 10:30:36 +0900

    Add debug message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,6 +11,8 @@ import "prismjs/themes/prism.css"
 const onRedirectCallback = appState => navigate(appState?.returnTo || "/")
 
 export const wrapRootElement = ({ element }) => {
+  console.log(`AUTH0_DOMAIN: ${process.env.AUTH0_DOMAIN}`)
+  console.log(`GATSBY_DOMAIN: ${process.env.GATSBY_DOMAIN}`)
   return (
     <Auth0Provider
       domain={process.env.AUTH0_DOMAIN}
