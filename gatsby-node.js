803280f7a0ad74b2e23802a03ef5a00fe2531130
Seokmin Hong <kyma123@unist.ac.kr>
2020-09-07 23:57:16 +0900

    Replace require to got and store JWT on .env

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,40 @@
 const path = require(`path`)
-const request = require("request")
+const got = require(`got`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// Get JWT from Authorization servece.
+const getToken = async () => {
+  const { body } = await got.post(
+    `https://${process.env.GATSBY_DOMAIN}/oauth/token`,
+    {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      form: {
+        grant_type: "client_credentials",
+        client_id: process.env.GATSBY_API_ID,
+        client_secret: process.env.GATSBY_API_SECRET,
+        audience: process.env.GATSBY_AUDIENCE,
+      },
+    }
+  )
+  if (process.env.NODE_ENV === "development") {
+    console.log(`New Access token is assigned:\n${body.access_token}`)
+  }
+  return body.access_token
+}
+
+// Post an article.
+const postArticle = (post, token) =>
+  got.post(`${process.env.GATSBY_API_DOMAIN}/articles`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: post.node.fields.slug.replace(/^\/([\w-]+)\/$/, "$1"),
+    }),
+  })
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -35,6 +69,22 @@ exports.createPages = async ({ graphql, actions }) => {
   // Create blog posts pages.
   const posts = result.data.allMarkdownRemark.edges
 
+  let token = process.env.GATSBY_ACCESS_TOKEN ?? ""
+  // Check is the token valid.
+  await got
+    .post(`${process.env.GATSBY_API_DOMAIN}/auth`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .catch(async reason => {
+      if (reason.response.statusCode === 401) {
+        token = await getToken()
+        return
+      }
+      throw reason
+    })
+
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
@@ -48,41 +98,7 @@ exports.createPages = async ({ graphql, actions }) => {
         next,
       },
     })
-  })
-
-  var options = {
-    method: "POST",
-    url: `https://${process.env.GATSBY_DOMAIN}/oauth/token`,
-    headers: { "content-type": "application/x-www-form-urlencoded" },
-    form: {
-      grant_type: "client_credentials",
-      client_id: process.env.GATSBY_API_ID,
-      client_secret: process.env.GATSBY_API_SECRET,
-      audience: process.env.GATSBY_AUDIENCE,
-    },
-  }
-
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error)
-
-    posts.forEach(post => {
-      console.log(post.node.fields.slug.replace(/^\/([\w-]+)\/$/, "$1"))
-      let options = {
-        method: "POST",
-        url: `${process.env.GATSBY_API_DOMAIN}/articles`,
-        headers: {
-          Authorization: `Bearer ${JSON.parse(body).access_token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id: post.node.fields.slug.replace(/^\/([\w-]+)\/$/, "$1"),
-        }),
-      }
-      request(options, (error, response) => {
-        if (error) throw new Error(error)
-        console.log(response.body)
-      })
-    })
+    postArticle(post, token)
   })
 }
 
